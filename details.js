300a50893c28ca39db91fe7bcd180c7fd674fca7
kashyapndps00 <98746601+kashyapndps00@users.noreply.github.com>
2022-04-10 22:48:05 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: '5166228737:AAGNGYOOX6chNLMA9KMFvnR-zZq_GYq470E',
-    admins: [5035811352],
+    token: '5100261201:AAHbHHjlx6p3DhOspVFhrCiHg7R3PwRpVnk',
+    admins: [5215163224],
     curr:'INR'
 }
 
