913f3f1227cbef43b000f8fca201cc892cd07c65
Kashyap Sharma <98746601+kashyapndps00@users.noreply.github.com>
2022-04-11 17:18:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -278,7 +278,7 @@ bot.hears('💰 Account' , async (ctx) =>{
         }else{
             var wallet = data[0].wallet
         }
-        let text = "*💁User = "+ctx.from.first_name+"\n\n💰 Your Balance = "+bal.toFixed(3)+" "+curr+"\n\n🗂️Wallet = *`"+wallet+"`"
+        let text = "*🙌🏻 User = "+ctx.from.first_name+"\n\n💰 Balance = "+bal.toFixed(3)+" "+curr+"\n\n🗂️Wallet = *`"+wallet+"`\n\n🪢 Invite To Earn More"
         ctx.replyWithMarkdown(text)
     }catch(e){
         console.log(e)
