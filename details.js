61c2c813c0da4d17a7747ec14e9dbf54f94e023c
Kashyap Sharma <98746601+kashyapndps00@users.noreply.github.com>
2022-04-20 21:53:35 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,6 +1,6 @@
 module.exports = {
     token: '5249293741:AAEEmbUu5PtGW62oRpzeFbqSy_mIa9L1NmQ',
-    admins: [5215163224,1150873291,1210366533],
+    admins: [5215163224,1210366533],
     curr:'INR'
 }
 
